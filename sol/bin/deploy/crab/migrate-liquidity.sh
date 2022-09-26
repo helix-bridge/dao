8f09037528f147014f7af43842964af477214148
echo <hujw77@gmail.com>
2022-09-26 16:18:41 +0800

    migrate USDT/xRING and WCRAB/xRING liquidity

--- sol/bin/deploy/crab/migrate-liquidity.sh
+++ sol/bin/deploy/crab/migrate-liquidity.sh
@@ -15,7 +15,7 @@ TIMELOCK=0x2401224012bAE7C2f217392665CA7abC16dCDE1e
 SALT=0x000000000000000000000000000000000000000000000000000000000000000e
 PREDECESSOR=0x0000000000000000000000000000000000000000000000000000000000000000
 DELAY=1800
-DEADLINE=1664186400
+DEADLINE=1664359200
 
 cal() {
   python3 -c "print(int($1))"
@@ -30,7 +30,8 @@ BALANCE1=$(seth call $xRING "balanceOf(address)(uint)" $USDT_xRING_PAIR)
 USDT_AMOUT_OUT=$(cal "$USDT_xRING_LIQ * $BALANCE0 / $TOTALSUPPLY0 * 0.99")
 xRING_AMOUT_OUT0=$(cal "$USDT_xRING_LIQ * $BALANCE1 / $TOTALSUPPLY0 * 0.99")
 
-data1=$(seth calldata "removeLiquidity(address,address,uint,uint,uint,address,uint)" $USDT $xRING $USDT_xRING_LIQ $USDT_AMOUT_OUT $xRING_AMOUT_OUT0 $TIMELOCK $DEADLINE)
+data1=$(seth calldata "approve(address,uint)" $ROUTER $USDT_xRING_LIQ)
+data2=$(seth calldata "removeLiquidity(address,address,uint,uint,uint,address,uint)" $USDT $xRING $USDT_xRING_LIQ $USDT_AMOUT_OUT $xRING_AMOUT_OUT0 $TIMELOCK $DEADLINE)
 
 WCRAB_xRING_LIQ=$(seth call $WCRAB_xRING_PAIR "balanceOf(address)(uint)" $TIMELOCK)
 TOTALSUPPLY1=$(seth call $WCRAB_xRING_PAIR "totalSupply()(uint)")
@@ -39,33 +40,37 @@ BALANCE1=$(seth call $xRING "balanceOf(address)(uint)" $WCRAB_xRING_PAIR)
 WCRAB_AMOUT_OUT=$(cal "$WCRAB_xRING_LIQ * $BALANCE0 / $TOTALSUPPLY1 * 0.99")
 xRING_AMOUT_OUT1=$(cal "$WCRAB_xRING_LIQ * $BALANCE1 / $TOTALSUPPLY1 * 0.99")
 
-data2=$(seth calldata "removeLiquidity(address,address,uint,uint,uint,address,uint)" $xRING $WCRAB $WCRAB_xRING_PAIR $xRING_AMOUT_OUT1 $WCRAB_AMOUT_OUT $TIMELOCK $DEADLINE)
+data3=$(seth calldata "approve(address,uint)" $ROUTER $WCRAB_xRING_PAIR)
+data4=$(seth calldata "removeLiquidity(address,address,uint,uint,uint,address,uint)" $WCRAB $xRING $WCRAB_xRING_PAIR $WCRAB_AMOUT_OUT $xRING_AMOUT_OUT1  $TIMELOCK $DEADLINE)
 
-MigrateBL=$(cal "$xRING_BL0 + $xRING_AMOUT_OUT0 + $xRING_AMOUT_OUT1")
+MigrateBL=$(cal "($xRING_BL0 + $xRING_AMOUT_OUT0 + $xRING_AMOUT_OUT1) * 1.1")
 
-data3=$(seth calldata "approve(address,uint)" $xRING_MIGRATOR $MigrateBL)
-data4=$(seth calldata "migrateAll()")
+data5=$(seth calldata "approve(address,uint)" $xRING_MIGRATOR $MigrateBL)
+data6=$(seth calldata "migrateAll()")
 
-data5=$(seth calldata "approve(address,uint)" $ROUTER $USDT_AMOUT_OUT)
-data6=$(seth calldata "approve(address,uint)" $ROUTER $(($xRING_AMOUT_OUT0 + $xRING_AMOUT_OUT1)))
-data7=$(seth calldata "approve(address,uint)" $ROUTER $WCRAB_AMOUT_OUT)
+data7=$(seth calldata "approve(address,uint)" $ROUTER $USDT_AMOUT_OUT)
+data8=$(seth calldata "approve(address,uint)" $ROUTER $(($xRING_AMOUT_OUT0 + $xRING_AMOUT_OUT1)))
+data9=$(seth calldata "approve(address,uint)" $ROUTER $WCRAB_AMOUT_OUT)
 
 USDT_MIN=$(cal "$USDT_AMOUT_OUT * 0.99")
 xWRING_MIN0=$(cal "$xRING_AMOUT_OUT0 * 0.99")
-data8=$(seth calldata "addLiquidity(address,address,uint,uint,uint,uint,address,uint)" $USDT $xWRING $USDT_AMOUT_OUT $xRING_AMOUT_OUT0 $USDT_MIN $xWRING_MIN0 $TIMELOCK $DEADLINE)
+data10=$(seth calldata "addLiquidity(address,address,uint,uint,uint,uint,address,uint)" $USDT $xWRING $USDT_AMOUT_OUT $xRING_AMOUT_OUT0 $USDT_MIN $xWRING_MIN0 $TIMELOCK $DEADLINE)
 
 WCRAB_MIN=$(cal "$WCRAB_AMOUT_OUT *0.99")
 xWRING_MIN1=$(cal "$xRING_AMOUT_OUT1 *0.99")
-data9=$(seth calldata "addLiquidity(address,address,uint,uint,uint,uint,address,uint)" $WCRAB $xWRING $WCRAB_AMOUT_OUT $xRING_AMOUT_OUT1 $WCRAB_MIN $xWRING_MIN1 $TIMELOCK $DEADLINE)
+data11=$(seth calldata "addLiquidity(address,address,uint,uint,uint,uint,address,uint)" $WCRAB $xWRING $WCRAB_AMOUT_OUT $xRING_AMOUT_OUT1 $WCRAB_MIN $xWRING_MIN1 $TIMELOCK $DEADLINE)
 
-targets=[$ROUTER,$ROUTER,$xRING,$xRING_MIGRATOR,$USDT,$xWRING,$WCRAB,$ROUTER,$ROUTER]
-values=[0,0,0,0,0,0,0,0,0]
-datas=[$data1,$data2,$data3,$data4,$data5,$data6,$data7,$data8,$data9]
+targets=[$USDT_xRING_PAIR,$ROUTER,$WCRAB_xRING_PAIR,$ROUTER,$xRING,$xRING_MIGRATOR,$USDT,$xWRING,$WCRAB,$ROUTER,$ROUTER]
+values=[0,0,0,0,0,0,0,0,0,0,0]
+datas=[$data1,$data2,$data3,$data4,$data5,$data6,$data7,$data8,$data9,$data10,$data11]
 
 data=$(seth calldata "scheduleBatch(address[],uint256[],bytes[],bytes32,bytes32,uint256)" $targets $values $datas $PREDECESSOR $SALT $DELAY)
 
 seth call -F $WALLET $TIMELOCK $data
 
-# seth send $WALLET "submitTransaction(address,uint,bytes)" $TIMELOCK 0 $data
+seth send $WALLET "submitTransaction(address,uint,bytes)" $TIMELOCK 0 $data
+
+count=$(seth call $WALLET "transactionCount()(uint)")
+seth call $WALLET "transactions(uint)(address,uint,bytes,bool)" $(( $count - 1 ))
 
 # seth call $TIMELOCK "executeBatch(address[],uint256[],bytes[],bytes32,bytes32)" $targets $values $datas $PREDECESSOR $SALT
