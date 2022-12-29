b65cbb5648f33a1f591e0addd23d91faab1b4fb3
echo <hujw77@gmail.com>
2022-12-29 17:34:57 +0800

    prerare for helix migration

---
+++ sol/bin/deploy/crab/upgrade-helix.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -eox pipefail
+
+WALLET=0x0050F880c35c31c13BFd9cBb7D28AafaEcA3abd2
+TIMELOCK=0x2401224012bAE7C2f217392665CA7abC16dCDE1e
+ADMIN=0x9FA16146BA5b7d144927E46acd9ba143f7205Fc8
+PROXY=0x8738A64392b71617aF4C685d0E827855c741fDF7
+LOGIC=0x31588a248550a90Bb6Bf784434e17D9470Ea0659
+
+SALT=0x0000000000000000000000000000000000000000000000000000000000000010
+PREDECESSOR=0x0000000000000000000000000000000000000000000000000000000000000000
+DELAY=1800
+
+target=$ADMIN
+value=0
+data1=$(seth calldata "upgrade(address,address)" "$PROXY" "$LOGIC")
+
+data=$(seth calldata "schedule(address,uint256,bytes,bytes32,bytes32,uint256)" $target $value $data1 $PREDECESSOR $SALT $DELAY)
+
+seth call -F $WALLET $TIMELOCK $data
+
+# seth send $WALLET "submitTransaction(address,uint,bytes)" $TIMELOCK 0 $data
+# seth send $TIMELOCK "execute(address,uint256,bytes,bytes32,bytes32)" $target $value $data1 $PREDECESSOR $SALT
---
+++ sol/bin/deploy/darwinia/upgrade-helix.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -eox pipefail
+
+WALLET=0xBd1a110ec476b4775c43905000288881367B1a88
+TIMELOCK=0xA546F42BeB3DEa617b0F3Ca6995C7Df5dfCaD29D
+ADMIN=0x91331B6Dd589163aF02Fb13E0466c3ba10eE310f
+PROXY=0xF3c1444CD449bD66Ef6DA7CA6c3E7884840A3995
+LOGIC=0x3141fEB844049B7b0f922542a4cf5Bcae9e1367d
+
+SALT=0x0000000000000000000000000000000000000000000000000000000000000003
+PREDECESSOR=0x0000000000000000000000000000000000000000000000000000000000000000
+DELAY=1800
+
+target=$ADMIN
+value=0
+data1=$(seth calldata "upgrade(address,address)" "$PROXY" "$LOGIC")
+
+data=$(seth calldata "schedule(address,uint256,bytes,bytes32,bytes32,uint256)" $target $value $data1 $PREDECESSOR $SALT $DELAY)
+
+seth call -F $WALLET $TIMELOCK $data
+
+# seth send $WALLET "submitTransaction(address,uint,bytes)" $TIMELOCK 0 $data
+# seth send $TIMELOCK "execute(address,uint256,bytes,bytes32,bytes32)" $target $value $data1 $PREDECESSOR $SALT
