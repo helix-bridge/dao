d0758d158d7ee14ac4621f445fa7eff5a901789c
echo <hujw77@gmail.com>
2022-12-30 09:37:46 +0800

    helix upgrade log on darwinia

--- sol/bin/deploy/darwinia/upgrade-helix.sh
+++ sol/bin/deploy/darwinia/upgrade-helix.sh
@@ -20,5 +20,9 @@ data=$(seth calldata "schedule(address,uint256,bytes,bytes32,bytes32,uint256)" $
 
 seth call -F $WALLET $TIMELOCK $data
 
-# seth send $WALLET "submitTransaction(address,uint,bytes)" $TIMELOCK 0 $data
+seth send $WALLET "submitTransaction(address,uint,bytes)" $TIMELOCK 0 $data
+
+count=$(seth call $WALLET "transactionCount()(uint)")
+seth call $WALLET "transactions(uint)(address,uint,bytes,bool)" $(( $count - 1 ))
+
 # seth send $TIMELOCK "execute(address,uint256,bytes,bytes32,bytes32)" $target $value $data1 $PREDECESSOR $SALT
