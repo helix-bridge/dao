9cd8f999ac120934d19d4439184138430b2b9a4a
echo <hujw77@gmail.com>
2023-03-03 14:32:07 +0800

    transfer 3M xWRING from Crab HelixDao to Darwinia HelixDaoMultiSig2

---
+++ sol/bin/deploy/crab/13-tranfer-xWRING.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -eox pipefail
+
+WALLET=0x0050F880c35c31c13BFd9cBb7D28AafaEcA3abd2
+TIMELOCK=0x2401224012bAE7C2f217392665CA7abC16dCDE1e
+xWRING=0x273131f7cb50ac002bdd08ca721988731f7e1092
+XIAOCH=0x88a39B052d477CfdE47600a7C9950a441Ce61cb4
+
+SALT=0x0000000000000000000000000000000000000000000000000000000000000013
+PREDECESSOR=0x0000000000000000000000000000000000000000000000000000000000000000
+DELAY=1800
+
+target=$xWRING
+value=0
+# transfer 3M xWRING to HelixDaoMultiSig2 on darwinia
+amount=$(seth --to-wei 3000000 ether)
+data1=$(seth calldata "transfer(address,uint)" "$XIAOCH" "$amount")
+
+data=$(seth calldata "schedule(address,uint256,bytes,bytes32,bytes32,uint256)" $target $value $data1 $PREDECESSOR $SALT $DELAY)
+
+seth call -F $WALLET $TIMELOCK $data
+seth send $WALLET "submitTransaction(address,uint,bytes)" $TIMELOCK 0 $data
+count=$(seth call $WALLET "transactionCount()(uint)")
+seth call $WALLET "transactions(uint)(address,uint,bytes,bool)" $(( $count - 1 ))
+
+# op_hash=$(seth call $TIMELOCK "hashOperation(address,uint256,bytes,bytes32,bytes32)(bytes32)" $target $value $data1 $PREDECESSOR $SALT)
+# date -r $(seth call $TIMELOCK "getTimestamp(bytes32)(uint)" $op_hash) '+%Y-%m-%d %H:%M:%S'
+
+# seth send $TIMELOCK "execute(address,uint256,bytes,bytes32,bytes32)" $target $value $data1 $PREDECESSOR $SALT
