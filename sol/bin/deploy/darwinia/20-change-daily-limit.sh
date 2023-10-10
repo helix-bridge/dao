0b7cc3ddd918ca2f961bc5ea3cd5e750b3f91eb4
echo <hujw77@gmail.com>
2023-10-10 11:15:24 +0800

    #20 execute

--- sol/bin/deploy/darwinia/20-change-daily-limit.sh
+++ sol/bin/deploy/darwinia/20-change-daily-limit.sh
@@ -19,8 +19,11 @@ data=$(seth calldata "schedule(address,uint256,bytes,bytes32,bytes32,uint256)" $
 
 # seth call -F $WALLET $TIMELOCK $data
 
-seth send $WALLET "submitTransaction(address,uint,bytes)" $TIMELOCK 0 $data --chain darwinia
-count=$(seth call $WALLET "transactionCount()(uint)" --chain darwinia)
-seth call $WALLET "transactions(uint)(address,uint,bytes,bool)" $(( $count - 1 )) --chain darwinia
+# seth send $WALLET "submitTransaction(address,uint,bytes)" $TIMELOCK 0 $data --chain darwinia
+# count=$(seth call $WALLET "transactionCount()(uint)" --chain darwinia)
+# seth call $WALLET "transactions(uint)(address,uint,bytes,bool)" $(( $count - 1 )) --chain darwinia
 
-# seth send $TIMELOCK "execute(address,uint256,bytes,bytes32,bytes32)" $target $value $data1 $PREDECESSOR $SALT
+op_hash=$(seth call $TIMELOCK "hashOperation(address,uint256,bytes,bytes32,bytes32)(bytes32)" $target $value $data1 $PREDECESSOR $SALT --chain darwinia)
+date -r $(seth call $TIMELOCK "getTimestamp(bytes32)(uint)" $op_hash --chain darwinia) '+%Y-%m-%d %H:%M:%S'
+
+seth send $TIMELOCK "execute(address,uint256,bytes,bytes32,bytes32)" $target $value $data1 $PREDECESSOR $SALT --chain darwinia
