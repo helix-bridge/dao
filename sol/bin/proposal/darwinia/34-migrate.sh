bf4ba3d8f97ed5a9a6349cd6496ddf503bf14589
echo <hujw77@gmail.com>
2024-04-01 17:32:52 +0800

    Migrate backing on darwinia

--- sol/bin/proposal/darwinia/34-migrate.sh
+++ sol/bin/proposal/darwinia/34-migrate.sh
@@ -2,6 +2,8 @@
 
 set -eox pipefail
 
+ETH_FROM=0x7aE77149ed38c5dD313e9069d790Ce7085caf0A6
+WALLET=0xBd1a110ec476b4775c43905000288881367B1a88
 TIMELOCK=0xa546f42beb3dea617b0f3ca6995c7df5dfcad29d
 WRING=0xe7578598aac020abfb918f33a20fad5b71d670b4
 KTON=0x0000000000000000000000000000000000000402
@@ -22,9 +24,15 @@ data2=$(seth calldata "rescueFunds(address,address,uint256)" $KTON  $NEW_BACKING
 
 datas=[$data1,$data2]
 
-seth calldata "scheduleBatch(address[],uint256[],bytes[],bytes32,bytes32,uint256)" $targets $values $datas $PREDECESSOR $SALT $DELAY
+data=$(seth calldata "scheduleBatch(address[],uint256[],bytes[],bytes32,bytes32,uint256)" $targets $values $datas $PREDECESSOR $SALT $DELAY)
+
+seth call -F $WALLET $TIMELOCK $data --chain darwinia
+
+# seth send -F $ETH_FROM $WALLET "submitTransaction(address,uint,bytes)" $TIMELOCK 0 $data --chain darwinia
+count=$(seth call $WALLET "transactionCount()(uint)" --chain darwinia)
+seth call $WALLET "transactions(uint)(address,uint,bytes,bool)" $(( $count - 1 )) --chain darwinia
 
 id=$(seth call $TIMELOCK "hashOperationBatch(address[],uint256[],bytes[],bytes32,bytes32)" $targets $values $datas $PREDECESSOR $SALT --chain darwinia)
 date -r $(seth call $TIMELOCK "getTimestamp(bytes32)(uint)" $id --chain darwinia) '+%Y-%m-%d %H:%M:%S'
 
-# seth send $TIMELOCK "executeBatch(address[],uint256[],bytes[],bytes32,bytes32)" $targets $values $datas $PREDECESSOR $SALT --chain darwinia
+# seth send -F $ETH_FROM $TIMELOCK "executeBatch(address[],uint256[],bytes[],bytes32,bytes32)" $targets $values $datas $PREDECESSOR $SALT --chain darwinia
