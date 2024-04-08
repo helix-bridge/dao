d4b354fdf9c8c511e369e19a676502a79fa1545e
echo <hujw77@gmail.com>
2024-04-08 21:31:14 +0800

    #36 remove lp on darwinia

--- sol/bin/proposal/darwinia/36-rm-lp.sh
+++ sol/bin/proposal/darwinia/36-rm-lp.sh
@@ -2,6 +2,8 @@
 
 set -eox pipefail
 
+ETH_FROM=0x7aE77149ed38c5dD313e9069d790Ce7085caf0A6
+WALLET=0xBd1a110ec476b4775c43905000288881367B1a88
 PAIR=0xdfA67E8f80F6B37a21e5C3baeEb0eCa893652Ab9
 ROUTER=0xB899409cDA0fFA2bF87F9c7b31f3c77D6A3a0bB0
 TIMELOCK=0xA546F42BeB3DEa617b0F3Ca6995C7Df5dfCaD29D
@@ -34,9 +36,13 @@ datas=[$data1,$data2]
 
 data=$(seth calldata "scheduleBatch(address[],uint256[],bytes[],bytes32,bytes32,uint256)" $targets $values $datas $PREDECESSOR $SALT $DELAY)
 
+seth call -F $WALLET $TIMELOCK $data --chain darwinia
 
-id=$(seth call $TIMELOCK "hashOperationBatch(address[],uint256[],bytes[],bytes32,bytes32)" $targets $values $datas $PREDECESSOR $SALT --chain darwinia)
-date -r $(seth call $TIMELOCK "getTimestamp(bytes32)(uint)" $id --chain darwinia) '+%Y-%m-%d %H:%M:%S'
+seth send -F $ETH_FROM $WALLET "submitTransaction(address,uint,bytes)" $TIMELOCK 0 $data --chain darwinia
+count=$(seth call $WALLET "transactionCount()(uint)" --chain darwinia)
+seth call $WALLET "transactions(uint)(address,uint,bytes,bool)" $(( $count - 1 )) --chain darwinia
 
+# id=$(seth call $TIMELOCK "hashOperationBatch(address[],uint256[],bytes[],bytes32,bytes32)" $targets $values $datas $PREDECESSOR $SALT --chain darwinia)
+# date -r $(seth call $TIMELOCK "getTimestamp(bytes32)(uint)" $id --chain darwinia) '+%Y-%m-%d %H:%M:%S'
+#
 # seth send -F $ETH_FROM $TIMELOCK "executeBatch(address[],uint256[],bytes[],bytes32,bytes32)" $targets $values $datas $PREDECESSOR $SALT --chain darwinia
-
