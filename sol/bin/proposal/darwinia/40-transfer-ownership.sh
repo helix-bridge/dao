92e59c009c4f8263b6e56a873c52d242a47f8317
echo <hujw77@gmail.com>
2024-06-13 09:20:41 +0800

    crab/32

---
+++ sol/bin/proposal/darwinia/40-transfer-ownership.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -eox pipefail
+
+TIMELOCK=0x000000000f681D85374225EdEeADC25560C1fb3F
+
+SALT=0x0000000000000000000000000000000000000000000000000000000000000000
+PREDECESSOR=0x0000000000000000000000000000000000000000000000000000000000000000
+DELAY=1800
+
+target=0x601dE3B81c7cE04BecE3b29e5cEe4F3251d250dB
+value=0
+data1=$(seth calldata 'transferOwnership(address)' 0xD0a0899c5dc2FEb253D57Ab0b7c6d1b1Fcbbf824)
+
+
+
+# data=$(seth calldata "schedule(address,uint256,bytes,bytes32,bytes32,uint256)" $target $value $data1 $PREDECESSOR $SALT $DELAY)
+
+seth call $TIMELOCK "execute(address,uint256,bytes,bytes32,bytes32)" $target $value $data1 $PREDECESSOR $SALT --chain crab
+
